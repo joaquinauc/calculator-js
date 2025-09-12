c1add7d849fa9045a5d8605bceca7aca5916284f
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-12 08:37:20 -0600

    Add function to add buttons to the calculator

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const BUTTON_QUANTITY = 16;
+const BUTTON_VALUES = 
+[
+    '7', '8', '9', '÷', 
+    '4', '5', '6', 'x',
+    '1', '2', '3', '-',
+    '0', '.', '+', '='
+];
+
+function addButtons()
+{
+    for (let i = 0; i < BUTTON_QUANTITY; i++)
+    {
+        const btnContainer = document.querySelector('.buttons-container');
+        let calculatorBtn = document.createElement('button');
+
+        calculatorBtn.setAttribute('value', `${BUTTON_VALUES[i]}`);
+        calculatorBtn.textContent = BUTTON_VALUES[i];
+
+        btnContainer.appendChild(calculatorBtn);
+    }
+}
+
+addButtons();
