3b3d9f14ac136febefe54370e70f4ce6dc436e49
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-15 13:43:10 -0600

    Add basic operation functions

--- calculator.js
+++ calculator.js
@@ -21,4 +21,24 @@ function addButtons()
     }
 }
 
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+function substract(num1, num2)
+{
+    return num1 - num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1 * num2;
+}
+
+function divide(num1, num2)
+{
+    return num1 / num2;
+}
+
 addButtons();
