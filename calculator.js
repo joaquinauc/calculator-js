8b1add6d69723259c106804cc59e1ef89108bcfe
joaquinauc <joaquins_urrea@hotmail.com>
2025-09-18 11:48:54 -0600

    Refactor event function to normal function

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 const BUTTON_QUANTITY = 16;
-const BUTTON_VALUES = 
-[
-    '7', '8', '9', '÷', 
-    '4', '5', '6', 'x',
-    '1', '2', '3', '-',
-    '0', '.', '+', '='
-];
+const BUTTON_VALUES =
+    [
+        '7', '8', '9', '÷',
+        '4', '5', '6', 'x',
+        '1', '2', '3', '-',
+        '0', '.', '+', '='
+    ];
 
 let screenContent = '';
 let calcOperator = '';
@@ -21,10 +21,8 @@ let numBtnPressed = false;
 let equalBtnPressed = false;
 let flagSecondNumber = false; // Without it, when trying to enter more than one digit after entering the first number, it won't let you, the number will keep being replaced.
 
-function addButtons()
-{
-    for (let i = 0; i < BUTTON_QUANTITY; i++)
-    {
+function addButtons() {
+    for (let i = 0; i < BUTTON_QUANTITY; i++) {
         const btnContainer = document.querySelector('#buttons-container');
         let calculatorBtn = document.createElement('button');
         calculatorBtn.setAttribute('class', 'calculator-btn');
@@ -36,32 +34,26 @@ function addButtons()
     }
 }
 
-function add(num1, num2)
-{
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function substract(num1, num2)
-{
+function substract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1, num2)
-{
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2)
-{
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, num2, operator)
-{
+function operate(num1, num2, operator) {
     let result = 0;
 
-    switch (operator)
-    {
+    switch (operator) {
         case '+':
             result = add(num1, num2);
             break;
@@ -79,8 +71,7 @@ function operate(num1, num2, operator)
     return parseFloat(result.toFixed(5));
 }
 
-function clearEverything()
-{
+function clearEverything() {
     screenContent = '';
     calcOperator = '';
     numberOnScreen = '';
@@ -111,6 +102,72 @@ function deleteLastEntry() {
     calculatorScreen.textContent = screenContent;
 }
 
+function calculatorButtonsFunctionality(btnStoredValue) {
+    const btnRealValue = parseFloat(btnStoredValue);
+
+    if (isNaN(btnRealValue)) {
+        switch (btnStoredValue) {
+            case '.':
+                if (!screenContent.includes('.')) screenContent += btnStoredValue;
+                dotBtnPressed = true;
+                break;
+            case '=':
+                if (!equalBtnPressed && opBtnPressed && (numberOffScreen !== '')) {
+                    secondNumber = parseFloat(screenContent);
+                    result = operate(firstNumber, secondNumber, calcOperator);
+                    numberOnScreen = result;
+                    screenContent = numberOnScreen;
+                    opBtnPressed = false;
+                    equalBtnPressed = true;
+                }
+                break;
+            default:
+                numberOnScreen = '0';
+                dotBtnPressed = false;
+                flagSecondNumber = true;
+
+                if (!opBtnPressed) {
+                    firstNumber = parseFloat(screenContent);
+
+                    if (isNaN(firstNumber)) {
+                        screenContent = '0';
+                        break;
+                    }
+
+                    numberOnScreen = firstNumber;
+                    screenContent = numberOnScreen;
+                    opBtnPressed = true;
+                    equalBtnPressed = false;
+                }
+                else if (opBtnPressed && numBtnPressed && (numberOffScreen !== '')) {
+                    secondNumber = parseFloat(screenContent);
+                    result = operate(firstNumber, secondNumber, calcOperator);
+                    numberOnScreen = result;
+                    screenContent = numberOnScreen;
+                    firstNumber = result;
+                    numBtnPressed = false;
+                }
+
+                numberOffScreen = '';
+                calcOperator = btnStoredValue;
+                break;
+        }
+    }
+    else {
+        if (screenContent === '0' || (opBtnPressed && !dotBtnPressed && flagSecondNumber) || equalBtnPressed) screenContent = '';
+        equalBtnPressed = false;
+        screenContent += btnStoredValue;
+        numberOffScreen += btnStoredValue;
+        numBtnPressed = true;
+        flagSecondNumber = false;
+    }
+
+    if (screenContent == '')
+        calculatorScreen.textContent = '0';
+    else
+        calculatorScreen.textContent = screenContent;
+}
+
 addButtons();
 
 const calculatorBtns = document.querySelectorAll('.calculator-btn');
@@ -121,80 +178,10 @@ const backspaceBtn = document.querySelector('#backspace-button');
 clearBtn.addEventListener('click', clearEverything);
 backspaceBtn.addEventListener('click', deleteLastEntry);
 
-for (let calculatorBtn of calculatorBtns)
-{
+for (let calculatorBtn of calculatorBtns) {
     calculatorBtn.addEventListener('click', () => {
         const btnStoredValue = calculatorBtn.getAttribute('value');
-        const btnRealValue = parseFloat(btnStoredValue);
-        
-        if (isNaN(btnRealValue))
-        {
-            switch (btnStoredValue)
-            {
-                case '.':
-                    if (!screenContent.includes('.')) screenContent += btnStoredValue;
-                    dotBtnPressed = true;
-                    break;
-                case '=':
-                    if (!equalBtnPressed && opBtnPressed && (numberOffScreen !== ''))
-                    {
-                        secondNumber = parseFloat(screenContent);
-                        result = operate(firstNumber, secondNumber, calcOperator);
-                        numberOnScreen = result;
-                        screenContent = numberOnScreen;
-                        opBtnPressed = false;
-                        equalBtnPressed = true;
-                    }
-                    break;
-                default:
-                    numberOnScreen = '0';
-                    dotBtnPressed = false;
-                    flagSecondNumber = true;
-
-                    if (!opBtnPressed)
-                    {
-                        firstNumber = parseFloat(screenContent);
-                        
-                        if (isNaN(firstNumber))
-                        {
-                            screenContent = '0';
-                            break;
-                        }
-
-                        numberOnScreen = firstNumber;
-                        screenContent = numberOnScreen;
-                        opBtnPressed = true;
-                        equalBtnPressed = false;
-                    }
-                    else if (opBtnPressed && numBtnPressed && (numberOffScreen !== ''))
-                    {
-                        secondNumber = parseFloat(screenContent);
-                        result = operate(firstNumber, secondNumber, calcOperator);
-                        numberOnScreen = result;
-                        screenContent = numberOnScreen;
-                        firstNumber = result;
-                        numBtnPressed = false;
-                    }
-
-                    numberOffScreen = '';
-                    calcOperator = btnStoredValue;
-                    break;
-            }
-        }
-        else
-        {
-            if (screenContent === '0' || (opBtnPressed && !dotBtnPressed && flagSecondNumber) || equalBtnPressed) screenContent = '';
-            equalBtnPressed = false;
-            screenContent += btnStoredValue;
-            numberOffScreen += btnStoredValue;
-            numBtnPressed = true;
-            flagSecondNumber = false;
-        }
-
-        if (screenContent == '')
-            calculatorScreen.textContent = '0';
-        else
-            calculatorScreen.textContent = screenContent;
+        calculatorButtonsFunctionality(btnStoredValue);
     });
 }
 
